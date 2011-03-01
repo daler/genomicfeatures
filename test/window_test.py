067ceaafe0880770208bcd5553b8f47644c08fb4
daler <dalerr@niddk.nih.gov>
2011-03-01 18:37:26 -0500

    minor tweaks to window testing

--- test/window_test.py
+++ test/window_test.py
@@ -15,19 +15,25 @@ if sys.argv[1] == 'bed3':
 if sys.argv[1] == 'sam':
     interval_file = _genomicfeatures.SAMFile(os.path.join(this_dir, 'example.sam'))
 if sys.argv[1] == 'bam':
-    fn = '/DATA/analysis/shep-ripseq/shep3_hs-ip/shep3_hs-ip.bam'
-    #fn 'example.bam'
+    fn = '/DATA/analysis/suhw-kc-ripseq/suhw1-ip/suhw1-ip.tophat.filtered.bam'
+    #fn = 'example.bam'
     interval_file = _genomicfeatures.BAMFile(os.path.join(this_dir, fn))
 
 
 fout = open('test.bedgraph','w')
 fout.write('track type=bedGraph name=score_test\n')
-window = _genomicfeatures.Window(interval_file, debug=0, limit=-1, halfwidth=72)
-for w in window:
-    score = _genomicfeatures.dups_score(w, window.halfwidth)
-    center = w[0].start + window.halfwidth
-    fout.write('\t'.join([ w[0].chrom, str(center), str(center+1), str(score)])+'\n')
-    #fout.flush()
-fout.close()
+debug = 1
+if 1:
+    window = _genomicfeatures.Window(interval_file, debug=debug, limit=35, halfwidth=72)
+    c = 0
+    for w in window:
+        c += 1
+        score = _genomicfeatures.dups_score(w, window.halfwidth)
+        center = w[0].start + window.halfwidth
+        if debug:
+            print '  %s =========>output:' % c, center, score, 
+            print [i.start for i in w]
+        fout.write('\t'.join([ w[0].chrom, str(center), str(center+1), str(score)])+'\n')
+        fout.flush()
+    fout.close()
 
-    
