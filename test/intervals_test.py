49b4c89fd9db5dbd2b911e44f435c14da7658f66
daler <dalerr@niddk.nih.gov>
2011-03-07 10:07:32 -0500

    tweaks to interval test script

--- test/intervals_test.py
+++ test/intervals_test.py
@@ -7,15 +7,16 @@ import os
 this_file = os.path.abspath(__file__)
 this_dir = os.path.split(this_file)[0]
 
-gtf_fn = 'hg19-genes.gtf'
-bed_fn = 'hg19-genes.bed'
 c = 0
 limit = 100
 short = False
+
+if sys.argv[1] == 'gff':
+    interval_file = _genomicfeatures.GFFFile(os.path.join(this_dir, 'dm3.gff'))
 if sys.argv[1] == 'gtf':
-    interval_file = _genomicfeatures.GTFFile(os.path.join(this_dir, gtf_fn))
+    interval_file = _genomicfeatures.GTFFile(os.path.join(this_dir, 'hg19-genes.gtf'))
 if sys.argv[1] == 'bed':
-    interval_file = _genomicfeatures.BEDFile(os.path.join(this_dir, bed_fn))
+    interval_file = _genomicfeatures.BEDFile(os.path.join(this_dir, 'hg19-genes.bed'))
 if sys.argv[1] == 'bed3':
     interval_file = _genomicfeatures.BEDFile(os.path.join(this_dir, bed_fn+'3'))
 if sys.argv[1] == 'sam':
@@ -24,19 +25,29 @@ if sys.argv[1] == 'bam':
     interval_file = _genomicfeatures.BAMFile(os.path.join(this_dir, 'example.bam'))
 
 for feature in interval_file:
-    """
-    if c > limit:
-        break
-    c += 1
-    """
-    #feature.midpoint()
     pass
 
+# generic attributes
 print feature
-print 'strand:', feature.strand
-print 'TSS:',feature.tss()
-print 'nfields:', feature.nfields
-print 'len:', len(feature)
-print 'alignments:', feature.alignments
-print 'cigar:', feature.pysam_read.cigar
-print 'tags:', feature.tags
+print '\tstrand:', feature.strand
+print '\tTSS:',feature.tss()
+print '\tnfields:', feature.nfields
+print '\tlen:', len(feature)
+print '\tstart:', feature.start
+print '\tstop:', feature.stop
+print '\tmidpoint:', feature.midpoint
+
+
+if sys.argv[1] in ['gtf','gff']:
+    print 
+    print 'specific to GTF and GFF'
+    print '\tattributes:', feature.attributes
+    print '\tmethod:', feature.method
+    print '\tscore:', feature.score
+
+if sys.argv[1] in ['bam','sam']:
+    print
+    print 'specific to BAM and SAM'
+    print '\talignments:', feature.alignments
+    print '\tcigar:', feature.pysam_read.cigar
+    print '\ttags:', feature.tags
