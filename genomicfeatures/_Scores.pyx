b4b8d01a933266c4b4ef08bf8679a9c6b531b476
daler <dalerr@niddk.nih.gov>
2011-04-08 15:39:13 -0400

    better-defined scoring

--- genomicfeatures/_Scores.pyx
+++ genomicfeatures/_Scores.pyx
@@ -77,7 +77,7 @@ cpdef float dups_score(object x, int halfwidth, float scalar=1) except -1:
     *scalar* is what each score should be mutliplied by (e.g., a scale factor
     for reads per million mapped)
 
-    Score is calculated by taking the sum of reads in the center and iding
+    Score is calculated by taking the sum of reads in the center and dividing
     by average number of of non-zero and non-center duplicates.
 
     Window size is assumed to be (x[0].start + halfwidth).
@@ -101,7 +101,7 @@ cpdef float dups_score(object x, int halfwidth, float scalar=1) except -1:
         avg nonzero duplicates = (3 reads at pos 0) + (2 reads at pos 4) + (1 read at pos 8)  
                                  ___________________________________________________________  = 2.0
                                                   3 positions with >0 reads
-                                                  
+
         score at pos 5 = 6 / 2.0 = 3.0
 
 
@@ -139,6 +139,8 @@ cpdef float dups_score(object x, int halfwidth, float scalar=1) except -1:
     # array([3, 0, 0, 0, 2, 6, 0, 0, 1])
     #        ^                       ^
     #    '0' was found 3 times       '8' was found once
+    #
+    # This is the number of duplicates at each location.
     
 
     cdef np.ndarray[np.int_t, ndim=1] dup_counts = np.bincount(starts)
@@ -152,12 +154,15 @@ cpdef float dups_score(object x, int halfwidth, float scalar=1) except -1:
 
     # Imagine for the example above that halfwidth is 6 
     # There are (2*halfwidth - len(dup_counts) ) zeros that could be appended on the end.
+    # 
+    # Here's what dup_counts might look like if there are only reads 9
+    # positions into the window:
     #
     # dup_counts = [3, 0, 0, 0, 2, 6, 0, 0, 1]
-
+    #
     # What it would look like if we took the time to actually pad it:
     # padded     = [3, 0, 0, 0, 2, 6, 0, 0, 1, 0, 0, 0]
-    
+    #
     # But instead let's just get how many extra zeros there should be
     padding = 2*halfwidth - dc_len
     
@@ -166,18 +171,23 @@ cpdef float dups_score(object x, int halfwidth, float scalar=1) except -1:
     # the case of a single read all by its lonesome, with no other reads in the
     # window
 
-    # add 1 to everything we have a value for so far
-    dup_counts += 1
+    # Originally, I was adding 1 to everything we have a value for so far:
+    #
+    # dup_counts += 1
+    #
+    # but really only the window average should be +1
 
-    # number of actual center duplicates plus 1
-    center_dups = dup_counts[halfwidth] 
+    # number of actual center duplicates
+    center_dups = dup_counts[halfwidth]
     
     # the padding is added cause it's like we're adding padding * 1 reads to
     # the window total.
-    non_center_dups = dup_counts.sum() + padding - center_dups
 
-    avg_dups = non_center_dups / (dc_len - 1) 
-    score = center_dups / avg_dups * scalar
+    # set the center to be zero
+    dup_counts[halfwidth] = 0
+
+    avg_dups = dup_counts.mean() 
+    score = center_dups / (avg_dups+1) * scalar
     return score
     
 
